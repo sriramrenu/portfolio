482971a54e3e465c279bc5ea62fe3f28334034ed
Sriram R <r.sriramrenu@gmail.com>
2026-04-13 22:51:54 +0530

    Delete components directory

--- components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="border-t border-border py-8 bg-background/50">
-      <div className="max-w-5xl mx-auto px-6 text-center text-foreground/60 text-sm">
-        <p>© 2025 All rights reserved. Crafted with passion and code.</p>
-      </div>
-    </footer>
-  )
-}
